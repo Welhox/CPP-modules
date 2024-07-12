d2d16bb938f7f6acebd8ee9cbd5181f24dc44c95
Casimir Lundberg <clundber@c1r5p20.hive.fi>
2024-07-12 14:34:12 +0300

    finished

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -5,25 +5,28 @@ int main(int argc, char *argv[])
 {
     int i = 1;
     int x = 0;
-    if (argc < 2)
-    {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    }
-    else
+    bool    empty = true;
+
+    //if (argc < 2)
+    //    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    if (argc >= 2)
     {
         while (argv[i])
         {
             x = 0;
             while (argv[i][x])
             {
-                if (argv[i][x] >= 'a' && argv[i][x] <= 'z')
-                    argv[i][x] -= 32;       
+                argv[i][x] = toupper(argv[i][x]);
                 x++;
             }
+            if (x > 0)
+                empty = false;
             std::cout << argv[i];
             i++;
         }
     }
+    if (empty == true)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     std::cout << "\n";
     return (0);
 }
